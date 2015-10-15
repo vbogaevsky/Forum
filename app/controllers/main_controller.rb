98a9c060394895ebddc946196acc0507bd22cea9
Vladimir Bogaevsky <gitvbogaevsky@gmail.com>
2015-10-16 01:20:39 +0300

    Adds newBoard method to main_controller and to index

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,4 +2,8 @@ class MainController < ApplicationController
   def index
   	@boards = Board.all
   end
+  def newBoard
+  	board = Board.create(params.require(:something).permit(:status))
+  	@boards << board
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'main#index'
-
+  get 'main/newBoard' => 'main#newBoard'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
